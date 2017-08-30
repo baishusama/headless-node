c2802bf003734b7574de68d16fcec2a6ba9a950d
baishusama <baishusama@outlook.com>
2017-08-30 08:18:37 +0800

    initialized project

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var superagent = require('superagent')
+var cheerio = require('cheerio')
+
+var app = express()
+
+app.get('/', function(req, res, next){
+    /*console.log("req : ", req)
+    console.log("res : ", res)
+    console.log("next : ", next)*/
+
+    superagent.get('https://cnodejs.org/')
+        .end(function(err, sres){
+            if(err) {
+                return next(err)
+            }
+
+            console.log(sres)
+            // console.log(sres.text)
+
+            var $ = cheerio.load(sres.text)
+            var items = []
+
+        })
+})
+
+// process.env.PORT for deploy to heroku.com
+app.listen(2333, function(){
+    console.log('app is running at port 2333');
+})
